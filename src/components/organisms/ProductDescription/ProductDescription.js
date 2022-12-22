725d8ea0f6688df1250737a558556b24ba423f97
makrenka <smokrenko79@gmail.com>
2022-12-22 23:03:00 +0300

    value calculation for rating

--- src/components/organisms/ProductDescription/ProductDescription.js
+++ src/components/organisms/ProductDescription/ProductDescription.js
@@ -1,6 +1,40 @@
 import { Component } from "../../../core/Component/Component";
+import { databaseService } from "../../../services/Database";
 
 export class ProductDescription extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isLoading: false,
+            comments: [],
+        }
+    }
+
+    toggleIsLoading() {
+        this.setState((state) => {
+            return {
+                ...state,
+                isLoading: !state.isLoading,
+            }
+        })
+    }
+
+    getComments() {
+        this.toggleIsLoading();
+        databaseService.read("comments")
+            .then((data) => {
+                this.setState((state) => {
+                    return {
+                        ...state,
+                        comments: data,
+                    }
+                });
+
+            })
+            .finally(() => {
+                this.toggleIsLoading();
+            })
+    }
 
     static get observedAttributes() {
         return [
@@ -14,32 +48,48 @@ export class ProductDescription extends Component {
         ];
     }
 
+    componentDidMount() {
+        this.getComments();
+    }
+
+    componentWillUnmount() {
+        this.getComments();
+    }
+
     render() {
+        const filt = this.state.comments.filter(item => item.product == this.props.title);
+        const avrGrade = filt.map(({ grade }) => Number(grade))
+            .reduce((sum, current) => sum + current, 0) / filt.length;
 
         return `
-        
-        <div class="product-main__product-section-options">
-            <mrd-product-description-heading
-                title="${this.props.title}"
-                price="${this.props.price}"
-                discount="${this.props.discount}"
-            >
-            </mrd-product-description-heading>
+        <mrd-preloader is-loading="${this.state.isLoading}">
+            <div class="product-main__product-section-options">
+                <mrd-product-description-heading
+                    title="${this.props.title}"
+                    price="${this.props.price}"
+                    discount="${this.props.discount}"
+                >
+                </mrd-product-description-heading>
 
-            <mrd-product-rating></mrd-product-rating>
-            <mrd-product-colors></mrd-product-colors>
-            <mrd-button-add-to-cart></mrd-button-add-to-cart>
-            <mrd-product-paying></mrd-product-paying>
-            <mrd-product-delivery></mrd-product-delivery>
+                <mrd-product-rating
+                    grade="${avrGrade}"
+                >
+                </mrd-product-rating>               
 
-            <mrd-delivery-description
-                delivery="${this.props.delivery}"
-                dimensions="${this.props.dimensions}"
-                finance="${this.props.finance}"
-                includes="${this.props.includes}"
-            >
-            </mrd-delivery-description>
-        </div>
+                <mrd-product-colors></mrd-product-colors>
+                <mrd-button-add-to-cart></mrd-button-add-to-cart>
+                <mrd-product-paying></mrd-product-paying>
+                <mrd-product-delivery></mrd-product-delivery>
+
+                <mrd-delivery-description
+                    delivery="${this.props.delivery}"
+                    dimensions="${this.props.dimensions}"
+                    finance="${this.props.finance}"
+                    includes="${this.props.includes}"
+                >
+                </mrd-delivery-description>
+            </div>
+        </mrd-preloader>
         `
     }
 }
